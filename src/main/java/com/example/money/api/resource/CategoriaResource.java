3a04b1851ee0d1fcb52afb2cb10e8f546b4d4111
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-05-09 21:15:18 -0300

    Implementação de criação de nova categoria com POST e busca por código

--- src/main/java/com/example/money/api/resource/CategoriaResource.java
+++ src/main/java/com/example/money/api/resource/CategoriaResource.java
@@ -1,12 +1,23 @@
 package com.example.money.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.money.api.model.Categoria;
 import com.example.money.api.repository.CategoriaRepository;
@@ -21,6 +32,22 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}") //pega da requisição atual 
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri(); // o códico 
+		response.setHeader("Location", uri.toASCIIString()); //Seta no location  -> http://localhost:8080/categorias/32
+		
+		return ResponseEntity.created(uri).body(categoria);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo){
+		return categoriaRepository.findOne(codigo);
 	}
 	
 }
