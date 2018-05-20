cb67c36a4e59efa5d7c8aff002ecf57b2bcf7793
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-05-19 23:32:13 -0300

    Adição do nome do Usuário no token JWT

--- src/main/java/com/example/money/api/config/AutorizationServerConfig.java
+++ src/main/java/com/example/money/api/config/AutorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.example.money.api.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +10,13 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import com.example.money.api.config.token.CustomTokenEnhancer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -39,8 +45,12 @@ public class AutorizationServerConfig extends AuthorizationServerConfigurerAdapt
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+		
 		endpoints
 			.tokenStore(tokenStore())
+			.tokenEnhancer(tokenEnhancerChain)
 			.reuseRefreshTokens(false) //sempre que eu pedir um novo refresh_token será usado
 			.accessTokenConverter(accessTokenConverter())
 			.authenticationManager(authenticationManager);
@@ -56,6 +66,11 @@ public class AutorizationServerConfig extends AuthorizationServerConfigurerAdapt
 	@Bean
 	public TokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
+	}
+	
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+	    return new CustomTokenEnhancer();
 	}
 	
 }
